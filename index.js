652de5c7bfd38e8644827d8cb2c1c73755d398ec
roncli <roncli@roncli.com>
2025-03-29 17:23:26 -0700

    Package updates.

--- index.js
+++ index.js
@@ -1,87 +1,77 @@
 /**
- * @typedef {import("mssql").config} Mssql.config
- * @typedef {import("mssql").ConnectionPool} Mssql.ConnectionPool
  * @typedef {{[x: string]: {type: import("mssql").ISqlTypeFactoryWithNoParams | import("mssql").ISqlTypeWithLength | import("mssql").ISqlTypeWithNoParams | import("mssql").ISqlTypeWithPrecisionScale | import("mssql").ISqlTypeWithScale | import("mssql").ISqlTypeWithTvpType, value: any}}} Params
  */
 
 const mssql = require("mssql"),
     {PreparedStatement, TYPES} = mssql;
 
-/** @type {Promise} */
-let poolQueue = Promise.resolve();
-
-//  ####           #            #
-//   #  #          #            #
-//   #  #   ###   ####    ###   # ##    ###    ###    ###
-//   #  #      #   #         #  ##  #      #  #      #   #
-//   #  #   ####   #      ####  #   #   ####   ###   #####
-//   #  #  #   #   #  #  #   #  ##  #  #   #      #  #
-//  ####    ####    ##    ####  # ##    ####  ####    ###
+// MARK: class Database
 /**
 * Defines the database class.
 */
 class Database {
-    //                           #                       #
-    //                           #                       #
-    //  ##    ##   ###    ###   ###   ###   #  #   ##   ###    ##   ###
-    // #     #  #  #  #  ##      #    #  #  #  #  #      #    #  #  #  #
-    // #     #  #  #  #    ##    #    #     #  #  #      #    #  #  #
-    //  ##    ##   #  #  ###      ##  #      ###   ##     ##   ##   #
+    /** @type {Promise} */
+    static #poolQueue = Promise.resolve();
+
+    /** @type {mssql.ConnectionPool} */
+    #pool;
+
+    /** @type {mssql.config} */
+    #settings;
+
+    // MARK: constructor
     /**
      * A constructor that creates a new database object with the necessary settings.
-     * @param {Mssql.config} settings The settings to use for the mssql module.
+     * @param {mssql.config} settings The settings to use for the mssql module.
      */
     constructor(settings) {
-        this.settings = settings;
+        this.#settings = settings;
     }
 
-    //              #    ###               ##
-    //              #    #  #               #
-    //  ###   ##   ###   #  #   ##    ##    #
-    // #  #  # ##   #    ###   #  #  #  #   #
-    //  ##   ##     #    #     #  #  #  #   #
-    // #      ##     ##  #      ##    ##   ###
-    //  ###
+    // MARK: async #getPool
     /**
      * Gets the connection pool to use.  Creates it if it doesn't exist.
-     * @returns {Promise<Mssql.ConnectionPool>} A promise that resolves with the retrieved connection pool.
+     * @returns {Promise<mssql.ConnectionPool>} A promise that resolves with the retrieved connection pool.
      */
-    getPool() {
-        return poolQueue = poolQueue.then(() => {}).catch(() => {}).then(async () => {
-            // Check to see if we already have a connection pool, if so, return it.
-            if (this.pool && !this.pool.connected) {
+    async #getPool() {
+        // Wait for the current promise in #poolQueue to resolve before proceeding.
+        await Database.#poolQueue;
+
+        // Reassign #poolQueue to a new promise to serialize the next operation and return it.
+        return Database.#poolQueue = (async () => {
+            // Check if a connection pool exists but is not connected, close it.
+            if (this.#pool && !this.#pool.connected) {
                 await mssql.close();
-                delete this.pool;
+                this.#pool = void 0;
             }
 
-            if (this.pool && this.pool.connected) {
-                return this.pool;
+            // If a connected pool already exists, return it.
+            if (this.#pool && this.#pool.connected) {
+                return this.#pool;
             }
 
-            // Ensure the settings have been setup first.
-            if (!this.settings) {
-                throw new Error("You haven't setup your settings yet!");
+            // Ensure the settings have been configured.
+            if (!this.#settings) {
+                throw new Error("Database settings are not configured. Please provide a valid config object.");
             }
 
-            // Connect to the database, close any existing connection, create a new connection pool, and return it.
+            // Close any existing connections and create a new connection pool.
             try {
                 await mssql.close();
-            } finally {}
+            } finally {
+                this.#pool = await mssql.connect(this.#settings);
+            }
 
-            return this.pool = await mssql.connect(this.settings);
-        });
+            return this.#pool;
+        })();
     }
 
-    //  ###  #  #   ##   ###   #  #
-    // #  #  #  #  # ##  #  #  #  #
-    // #  #  #  #  ##    #      # #
-    //  ###   ###   ##   #       #
-    //    #                     #
+    // MARK: async query
     /**
      * Executes a query.
      * @param {string} sql The SQL query.
      * @param {Params} [params] The parameters of the query.
-     * @return {Promise} A promise that resolves when the query is complete.
+     * @returns {Promise<mssql.IProcedureResult>} A promise that returns the result of the query.
      */
     async query(sql, params) {
         // If params haven't been sent, default them.
@@ -90,38 +80,36 @@ class Database {
         }
 
         // Setup a new prepared statement.
-        const ps = new PreparedStatement(await this.getPool());
+        const ps = new PreparedStatement(await this.#getPool());
 
-        // Add each parameter as an input to the prepared statement.
-        Object.keys(params).forEach((key) => {
-            ps.input(key, params[key].type);
-        });
+        try {
+            // Add each parameter as an input to the prepared statement.
+            Object.keys(params).forEach((key) => {
+                ps.input(key, params[key].type);
+            });
 
-        // Prepare the statement.
-        await ps.prepare(sql);
+            // Prepare the statement.
+            await ps.prepare(sql);
 
-        // Get the values to send to the query.
-        const paramMap = Object.keys(params).map((key) => [key, params[key].value]),
-            paramList = {};
+            // Get the values to send to the query.
+            const paramMap = Object.keys(params).map((key) => [key, params[key].value]),
+                paramList = {};
 
-        for (let index = 0, {length} = Object.keys(paramMap); index < length; index++) {
-            paramList[paramMap[index][0]] = paramMap[index][1];
+            for (let index = 0, {length} = Object.keys(paramMap); index < length; index++) {
+                paramList[paramMap[index][0]] = paramMap[index][1];
+            }
+
+            // Return the data.
+            return await ps.execute(paramList);
+        } finally {
+            // Unprepare the statement.
+            await ps.unprepare();
         }
-
-        // Retrieve the data.
-        const data = await ps.execute(paramList);
-
-        // Unprepare the statement.
-        await ps.unprepare();
-
-        // Return the data.
-        return data;
     }
 }
 
-Object.keys(TYPES).forEach((key) => {
-    const value = TYPES[key];
-
+// Copy the mssql types to the Database object.
+Object.entries(TYPES).forEach(([key, value]) => {
     Database[key] = value;
     Database[key.toUpperCase()] = value;
 });
